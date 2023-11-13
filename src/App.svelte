6cd02c26654bcd586d6a9f806e4e068358cfb157
Jonas <sturmente@proton.me>
2023-11-13 17:42:45 +0100

    Added footer

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,21 @@
   
 </script>
 
-<main>
-  Hello World
-</main>
+<div id="wrapper">
+  <main>
+    Hello World
+  </main>
+  
+  <footer>
+    <a href="https://www.flaticon.com/free-icons/playing-cards" title="playing cards icons">Playing cards icons created by Freepik - Flaticon</a>
+  </footer>
+  
+</div>
 
 <style>
-  
+  #wrapper {
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+  }
 </style>
