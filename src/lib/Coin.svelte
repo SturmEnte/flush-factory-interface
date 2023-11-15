c4fa0442aa134a208a37a89398f75e097a88fdda
Jonas <sturmente@proton.me>
2023-11-15 12:57:27 +0100

    Styling of coins

--- src/lib/Coin.svelte
+++ src/lib/Coin.svelte
@@ -2,6 +2,50 @@
 
 <div id="coin">
 	<button>+</button>
-	<div id="value">Value</div>
+	<div id="info">
+		<div id="value">Value:</div>
+		<div id="amount">Amount:</div>
+	</div>
 	<button>-</button>
 </div>
+
+<style>
+	#coin {
+		background: var(--secondary-dark);
+		border-radius: var(--border-radius);
+		width: 80px;
+		height: 120px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		width: min-content;
+		height: min-content;
+		box-sizing: border-box;
+		padding: var(--padding-small);
+	}
+
+	#info {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		margin: var(--padding-small) 0;
+	}
+
+	#value {
+		/* margin: var(--padding-small) 0; */
+	}
+
+	button {
+		border: none;
+		background: var(--accent);
+		border-radius: 360px;
+		width: 1rem;
+		height: 1rem;
+		display: flex;
+		align-content: center;
+		justify-content: center;
+		cursor: pointer;
+	}
+</style>
